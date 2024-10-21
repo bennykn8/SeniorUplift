376c2a50a99d40fa954ba041f06a9c8efb54ca01
unknown <sendrohan123@gmail.com>
2024-10-20 20:15:05 -0500

    updated the front page of the website

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Import Routing
 import {
--- react-front-end/src/pages/FrontPage.js
+++ react-front-end/src/pages/FrontPage.js
@@ -1,22 +1,77 @@
 import React from 'react';
+import './FrontPage.css';  // Assuming the CSS file is named FrontPage.css
+import { Carousel } from 'react-bootstrap';
 
 const FrontPage = () => {
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-6 text-center mt-5">
-          <h1>Welcome to SeniorUpLift</h1>
+    <div className="container-fluid">
+      <div className="hero-section">
+        <div className="hero-text text-center">
+          <h1>Welcome to SeniorUplift</h1>
           <p>
-            SeniorUplift helps the elderly in Texas find nearby healthcare centers,
-            nursing homes, and entertainment, offering details like ratings
-            and proximity for easy access to services.
+            Helping the elderly in Texas find nearby healthcare centers, nursing homes,
+            and entertainment options with ratings and proximity for easy access to services.
           </p>
-          {/* Adding an image of elderly people */}
-          <img 
-            src="https://compote.slate.com/images/66168178-3547-4917-8ea9-12938af61a04.jpg" 
-            alt="Elderly people enjoying life" 
-            className="img-fluid mt-4" 
+          <a href="#learn-more" className="cta-button">Learn More</a>
+        </div>
+      </div>
+
+      {/* Carousel Slideshow in a smaller container */}
+      <div className="carousel-container mt-5">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 carousel-img"
+              src="https://images.pexels.com/photos/3768131/pexels-photo-3768131.jpeg?cs=srgb&dl=pexels-olly-3768131.jpg&fm=jpg"
+              alt="Slide 1"
+            />
+          </Carousel.Item>
+          
+          <Carousel.Item>
+            <img
+              className="d-block w-100 carousel-img"
+              src="https://media.istockphoto.com/id/1413583709/photo/cheerful-senior-having-fun-while-taking-selfie-at-retirement-community.jpg?s=612x612&w=0&k=20&c=nYFvWrL80Zh9DCDbBJNO5DnUQQv9qliQfBjLfFcymCc="
+              alt="Slide 2"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100 carousel-img"
+              src="https://vistaliving.net/wp-content/uploads/2017/07/AdobeStock_136239519-1080x675.jpeg"
+              alt="Slide 3"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
+      <div className="row justify-content-center mt-5 features-section" id="learn-more">
+        <div className="col-md-4 text-center feature-item">
+          <img
+            src="https://media.gettyimages.com/id/183361838/photo/medical-center.jpg?s=612x612&w=gi&k=20&c=405YNZE_8YM3kx_mNIw4AqnNPVPln6oby6UcYEq1rCA="
+            alt="Elderly people enjoying life"
+            className="img-fluid rounded-circle feature-image"
           />
+          <h3 className="mt-3">Healthcare Centers</h3>
+          <p>Find the best healthcare centers nearby with ratings and reviews.</p>
+        </div>
+        <div className="col-md-4 text-center feature-item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzVx_1TJ2qSPMFcF_cHPh32Bqx4z2-7x0z4g&s"
+            alt="Nursing Homes"
+            className="img-fluid rounded-circle feature-image"
+          />
+          <h3 className="mt-3">Nursing Homes</h3>
+          <p>Locate top-rated nursing homes within your vicinity for elderly care.</p>
+        </div>
+        <div className="col-md-4 text-center feature-item">
+          <img
+            src="https://fairviewadc.com/wp-content/uploads/2019/08/elderly-entertainment.jpg"
+            alt="Entertainment"
+            className="img-fluid rounded-circle feature-image"
+          />
+          <h3 className="mt-3">Entertainment</h3>
+          <p>Explore fun and engaging activities available for seniors in Texas.</p>
         </div>
       </div>
     </div>
