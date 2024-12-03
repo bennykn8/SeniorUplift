fcdc68a883a5027edb51fc620f245370b2a08743
unknown <sendrohan123@gmail.com>
2024-12-02 20:18:27 -0600

    fixed titles

--- react-front-end/src/pages/Visualizations.js
+++ react-front-end/src/pages/Visualizations.js
@@ -3,16 +3,24 @@ import BarChart from './BarChart';
 import HealthCenterBubblePlot from './BubbleChart';
 import BubbleMap from './BubbleMap';
 
-
 const Visualizations = () => {
+  const titleStyle = {
+    fontSize: '2rem', // Larger font size
+    fontWeight: 'bold', // Bold text
+    color: '#333', // Darker color for contrast
+    textTransform: 'uppercase', // Make the titles uppercase
+    margin: '20px 0', // Add spacing above and below
+    textDecoration: 'underline', // Add underline for emphasis
+  };
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <div style={{ marginBottom: '40px' }}> {/* Add margin-bottom */}
-        <h1>Event Categories</h1>
+      <div style={{ marginBottom: '40px' }}>
+        <h1 style={titleStyle}>Event Categories</h1>
         <BarChart />
-        <h1>Elderly Homes Locations</h1>
+        <h1 style={titleStyle}>Elderly Homes Locations</h1>
         <BubbleMap />
-        <h1>Most Frequent Hospital Locations</h1>
+        <h1 style={titleStyle}>Most Frequent Hospital Locations</h1>
         <HealthCenterBubblePlot />
       </div>
     </div>
